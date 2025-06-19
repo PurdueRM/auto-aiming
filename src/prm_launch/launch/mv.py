7e8d9c8622f51746e0d146e643ec4cb1a7d33819
purduerm <rmc@purdue.edu>
2025-06-19 14:22:19 -0400

    add launch for just mv

---
+++ src/prm_launch/launch/mv.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+    share_dir = get_package_share_directory("prm_launch")
+    mv_config = os.path.join(share_dir, "config", "mv_red_bidc.yaml")
+
+    opencv_armor_detector_config = os.path.join(share_dir, "config", "opencv_armor_detector_red_bidc.yaml")
+
+    return LaunchDescription(
+        [
+            Node(package="mv_publisher", executable="MVCameraNode", parameters=[mv_config]),
+    ])
